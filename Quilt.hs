a24b4d91a04142bf6a3408e6e9efae8f80800080
Lucy Lyon <lyonlg@hendrix.edu>
2023-05-02 17:53:18 -0500

    progress

--- Quilt.hs
+++ Quilt.hs
@@ -9,64 +9,9 @@ import Parsing2
 import Text.Parsec.Error (Message(Message))
 import qualified Data.Map as M
 
-
-
  --------------------
  ---- Data Types ----        
  --------------------
-
--- data Quilt where
---     QuiltColor :: Color -> Quilt
---     QuiltCoord :: Coord -> Quilt 
---     Num :: Double -> Quilt -- needs to be integer or floating point
---     QuiltBool :: Bool -> Quilt
-
-data Quilt where
-    ColorLit :: Color -> Quilt 
-    Number   :: Double -> Quilt
-    Coord    :: Double -> Double -> Quilt      
-    BoolLit  :: Bool -> Quilt
-    LitColorTriple :: Quilt -> Quilt -> Quilt -> Quilt -- three numbers
-    BoolOp :: Quilt -> Quilt -- BoolLit -> BoolLit
-    Comparison :: Quilt -> Quilt -> Quilt -- number -> number -> boolLit
-    QuiltExpr ::  Quilt -> Quilt -> Quilt -> Quilt -> Quilt -- takes 4 bools and makes new bool, or 4 numbers and maks new num, etc. 
-    Bin :: Op -> Quilt -> Quilt -> Quilt 
-    deriving (Eq, Show)
-
-data Op where
-    Plus :: Op
-    Minus :: Op
-    Times :: Op
-    Divide :: Op 
-    deriving (Eq, Show)
-
--- arithmetic operators??
-
-data QuiltType where
-    TypeBool  :: QuiltType     -- represents Boolean values
-    TypeNum   :: QuiltType     -- represents floating-point numbers
-    TypeColor :: QuiltType     -- represents a triple of floating-point numbers
-    deriving (Eq, Show)
-
-
-    --       data Quilt where
-    -- ColorLit :: Color -> Quilt
-    -- NumberLit :: Double -> Quilt
-    -- BoolLit :: Bool -> Quilt
-    -- Triple :: Quilt -> Quilt -> Quilt -> Quilt
-    -- Param :: Coord -> Quilt
-    -- Bin :: Op -> Quilt -> Quilt -> Quilt
-    -- Un :: Op -> Quilt -> Quilt
-    -- If :: Quilt -> Quilt -> Quilt -> Quilt
-    -- QuiltOp :: Quilt -> Quilt -> Quilt -> Quilt -> Quilt
-    -- Var :: String -> Quilt
-    -- Let :: String -> Quilt -> Quilt -> Quilt
-    -- deriving (Show)
-
--- data Coord where
---     CoordX :: Coord
---     CoordY :: Coord
---     deriving (Show)
 
 -- | A color is a list of red, green, and blue values between 0.0 - 1.0.
 --   For example, [0,0,0] is black, [1,1,1] is white, [0.5, 0, 0.5] is a
@@ -76,8 +21,78 @@ type Color = [Double]
 -- | A quilt function produces a Color for any given location.  The
 --   parameters are x and y coordinates in the range [-1,1].
 type QuiltFun = Double -> Double -> Color
-    
 
+data Quilt where
+    ColorLit    :: Color -> Quilt  -- COLORLIT
+    Number      :: Double -> Quilt --NUMBERLOT
+    BoolLit     :: Bool -> Quilt --SAME 
+    RGB         :: Quilt -> Quilt -> Quilt -> Quilt -- three numbers: red, green, blue values --TRIPLE 
+    Con         :: Quilt -> Quilt -> Quilt -> Quilt -- bool bool else  --IF
+    Bin         :: Op -> Quilt -> Quilt -> Quilt 
+    Un          :: Op -> Quilt -> Quilt 
+    Coordinates :: Coord -> Quilt -- PARAM
+    QuiltOp     :: Quilt -> Quilt -> Quilt -> Quilt -> Quilt -- takes 4 bools and makes new bool, or 4 numbers and maks new num, etc. --QUILTOP
+    Var         :: String -> Quilt
+    Let         :: String -> Quilt -> Quilt -> Quilt 
+    deriving (Eq, Show)
+
+    -- add sin/etc. instead of let?
+
+data Coord where
+    X :: Coord
+    Y :: Coord
+    deriving (Eq, Show)
+
+data Op where               -- later change to binOp, boolOp 
+    Plus :: Op
+    Minus :: Op
+    Times :: Op
+    Divide :: Op 
+    Less  :: Op
+    LessEqual :: Op
+    Equal :: Op
+    NotEqual :: Op
+    Greater :: Op
+    GreaterEqual :: Op 
+    Neg :: Op
+    Not :: Op 
+    And :: Op 
+    Or :: Op 
+    deriving (Eq, Show)
+
+data Type where
+    TypeBool  :: Type     -- represents Boolean values
+    TypeNum   :: Type     -- represents floating-point numbers
+    TypeColor :: Type     -- represents a triple of floating-point numbers
+    deriving (Eq, Show)
+
+
+-- data Uop where
+--     Negate :: Uop
+--     Not :: Uop
+--     deriving (Eq, Show)
+
+-- data ArithOp where
+--     Plus :: ArithOp
+--     Minus :: ArithOp
+--     Times :: ArithOp
+--     Divide :: ArithOp
+--     deriving (Eq, Show)
+
+-- data CompOp where
+--     Less  :: CompOp
+--     LessEqual :: CompOp
+--     Equal :: CompOp
+--     NotEqual :: CompOp
+--     Greater :: CompOp
+--     GreaterEqual :: CompOp
+--     deriving (Eq, Show)
+
+-- data BoolOp where
+--     And :: BoolOp
+--     Or :: BoolOp 
+--     deriving (Eq, Show)
+    
  ---------------------
  ----- Parsers -------       
  ---------------------
@@ -105,41 +120,92 @@ float = getFloat lexer
 
 lexer :: TokenParser u
 lexer = makeTokenParser $ emptyDef
-    { reservedNames = [ "pink", "red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray" ] }
+    { reservedNames = [ "pink", "red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray", "x", "y", "if", "then", "else", "let", "in" ] }
 
 quilt :: Parser Quilt
 quilt = whiteSpace *> parseQuilt <* eof
 
+parseQuilt :: Parser Quilt
+parseQuilt = buildExpressionParser table parseQuiltAtom
+  where
+    table = [ [ Prefix (Un Neg       <$ reservedOp "-")
+            ,   Prefix (Un Not       <$ reservedOp "!") ]
+            , [ Infix (Bin Times     <$ reservedOp "*") AssocLeft      -- ^ is AssocRight so it follows math rules
+            ,   Infix (Bin Divide    <$ reservedOp "/") AssocLeft ]     -- ex. 2^3^4 = 2^(3^4) instead of (2^3)^4 
+            , [ Infix (Bin Plus      <$ reservedOp "+") AssocLeft
+            ,   Infix (Bin Minus     <$ reservedOp "-") AssocLeft ]
+            , [ Prefix (Un Not       <$ reservedOp "!") ]
+            , [ Infix (Bin Less      <$ reservedOp "<"  ) AssocNone
+            ,   Infix (Bin LessEqual <$ reservedOp "<=" ) AssocNone
+            ,   Infix (Bin Equal     <$ reservedOp "="  ) AssocNone
+            ,   Infix (Bin NotEqual  <$ reservedOp "!=" ) AssocNone
+            ,   Infix (Bin Greater   <$ reservedOp ">"  ) AssocNone
+            ,   Infix (Bin Greater   <$ reservedOp ">=" ) AssocNone ]
+            , [ Infix (Bin And       <$ reservedOp "&&" ) AssocRight
+            ,   Infix (Bin Or        <$ reservedOp "||" ) AssocRight ]
+              ]
+
 parseColorLit :: Parser Quilt
 parseColorLit =
-        makeColorLitParser "pink"
-    <|> makeColorLitParser "red"
-    <|> makeColorLitParser "orange"
-    <|> makeColorLitParser "yellow"
-    <|> makeColorLitParser "green"
-    <|> makeColorLitParser "blue"
-    <|> makeColorLitParser "purple"
-    <|> makeColorLitParser "black"
-    <|> makeColorLitParser "white"
-    <|> makeColorLitParser "gray"
+    ColorLit (toColor "pink")   <$ reserved "pink"    <|>
+    ColorLit (toColor "red")    <$ reserved "red"     <|>
+    ColorLit (toColor "orange") <$ reserved "orange"  <|>
+    ColorLit (toColor "yellow") <$ reserved "yellow"  <|>
+    ColorLit (toColor "green")  <$ reserved "green"   <|>
+    ColorLit (toColor "blue")   <$ reserved "blue"    <|>
+    ColorLit (toColor "purple") <$ reserved "purple"  <|>
+    ColorLit (toColor "black")  <$ reserved "black"   <|>
+    ColorLit (toColor "white")  <$ reserved "white"   <|>
+    ColorLit (toColor "gray")   <$ reserved "gray" 
 
+parseRGB :: Parser Quilt
+parseRGB =  RGB
+    <$> (reservedOp "["  *> parseQuilt)
+    <*  reservedOp  "," <*> parseQuilt
+    <*  reservedOp  "," <*> parseQuilt
+    <*  reservedOp  "]"
 
-
- 
 parseNumber :: Parser Quilt
 parseNumber = Number <$> (try float <|> fromIntegral <$> integer)
-
-makeColorLitParser :: String -> Parser Quilt
-makeColorLitParser s = (ColorLit $ toColor s) <$ reserved s
 
 parseBool :: Parser Quilt
 parseBool =
         BoolLit True  <$ reservedOp "True"
     <|> BoolLit False <$ reservedOp "False"
 
+parseCoordinates :: Parser Quilt
+parseCoordinates = Coordinates X <$ reserved "x" <|> Coordinates Y <$ reserved "y"
+
+parseCon :: Parser Quilt 
+parseCon = Con
+     <$> (reserved "if"     *> parseQuilt)
+     <*> (reserved "then"   *> parseQuilt)
+     <*> (reserved "else"   *> parseQuilt)
+
+parseQuiltOp :: Parser Quilt
+parseQuiltOp = QuiltOp <$  reservedOp "quilt" <*> parseQuilt <*> parseQuilt <*> parseQuilt <*> parseQuilt
+        
+parseVar :: Parser Quilt
+parseVar = Var <$> identifier
+
+parseLet :: Parser Quilt
+parseLet = Let 
+    <$> (reserved    "let" *> identifier)
+    <*> (reservedOp  "="   *> parseQuilt)
+    <*> (reserved    "in"  *> parseQuilt)
+
+
 parseQuiltAtom :: Parser Quilt
-parseQuiltAtom =  parseColorLit <|> parseNumber 
--- <|> parseBool
+parseQuiltAtom =  parseColorLit 
+ <|> parseNumber 
+ <|> parseBool
+ <|> parseRGB
+ <|> parseCoordinates
+ <|> parseCon
+ <|> parseQuiltOp
+ <|> parseVar
+ <|> parseLet
+ <|> parens parseQuilt
 
 toColor :: String -> Color
 toColor "pink"   = [1,   0,   1]
@@ -153,50 +219,98 @@ toColor "black"  = [0,   0,   0]
 toColor "white"  = [1,   1,   1]
 toColor "gray"   = [0.5, 0.5, 0.5]
 
+colorToBool :: Color -> Bool -- to bool 
+colorToBool (x:xs) = x == 1.0
+
+boolToColor :: Bool -> Color -- from bool
+boolToColor True  = [1, 1, 1]
+boolToColor False = [0, 0, 0]
+
 ---------------------
 --- Type Checking ---
 ---------------------
 
-type Ctx = M.Map String QuiltType
+type Ctx = M.Map String Type
 
--- checks if the 1st is a subtype of the 2nd 
-isSubtypeOf :: QuiltType -> QuiltType -> Bool -- CHANGE TO CHECK SUBTYPE
-isSubtypeOf TypeColor  TypeColor  = True
-isSubtypeOf TypeNum TypeNum = True
-isSubtypeOf TypeBool   TypeBool   = True
-isSubtypeOf TypeNum TypeColor  = True
-isSubtypeOf _        _        = False
+data TypeError where
+    DoLater :: TypeError
+    TypeMismatchError :: TypeError 
+    deriving (Show)
 
--- Try to separate type inference involving subtyping into two phases: first, recursively infer the types of subexpressions; second, 
--- make sure their types are appropriately compatible, and if so compute an appropriate output type. You will probably want one or 
--- more helper functions to do this.
+
+showTypeError :: TypeError -> String
+showTypeError DoLater = "undefined"
+showTypeError TypeMismatchError = "These types don't match"
+
+inferType :: Quilt -> Either TypeError ()
+inferType _ = Right () -- TODO
+
+checkSubType :: Type -> Type -> Bool 
+checkSubType TypeNum TypeColor = True   -- num is a subtype of color
+checkSubType type1 type2 
+    | type1 == type2           = True   -- if the two types are the same, counts as subtype
+    | otherwise                = False  -- anything else: not a subtype 
+
+
+-- typechecking: recursively infer the types of subexpressions
+-- then make sure types are compatible 
+-- then compute appropriate output type
+-- should make multiple functions & helpers for this 
 
 ---------------------
 ---- Interpretor ----
 ---------------------
+type Env = M.Map String QuiltFun
 
 data InterpError where
   UndefinedVar :: String -> InterpError
+  Other        :: InterpError
 
 showInterpError :: InterpError -> String
 showInterpError (UndefinedVar x) = x ++ "is undefined"
-
-type Env = M.Map String QuiltFun
+showInterpError Other            = "this error should never show up. uh oh." 
 
 interpQuilt :: Env -> Quilt -> Either InterpError QuiltFun
 interpQuilt env (ColorLit c) = Right $ \x y -> c
-interpQuilt env (Number z) = Right $ \x y -> [z,z,z]
+interpQuilt env (Number num) = Right $ \x y -> [num, num, num]
+interpQuilt env (BoolLit z) = Right $ \x y -> boolToColor z   
+interpQuilt env (RGB r g b) = interpQuilt env r >>= \r' -> interpQuilt env g >>= \g' -> interpQuilt env b >>= \b' 
+                                -> Right $ \x y -> [head $ r' x y, head $ g' x y, head $ b' x y]
+interpQuilt env (Con i t e) = interpQuilt env i >>= \i' -> interpQuilt env t >>= \t' -> interpQuilt env e >>= \e' 
+                                -> Right $ \x y -> if colorToBool $ i' x y then t' x y else e' x y
+interpQuilt env (Bin op quilt1 quilt2) = runBin op <$> interpQuilt env quilt1 <*> interpQuilt env quilt2
+    where
+        runBin Plus  = applyArithm (+)
+        runBin Minus = applyArithm (-)
+        runBin Times = applyArithm (*)
+        runBin Divide = applyArithm (/)
+        runBin Less = applyComp (<)
+        runBin LessEqual = applyComp (<=)
+        runBin Equal = applyComp (==)
+        runBin Greater = applyComp (>)
+        runBin GreaterEqual = applyComp (>=)
+        applyArithm op f1 f2 x y = zipWith op (f1 x y) (f2 x y)
+        applyComp op f1 f2 x y = boolToColor $ op (f1 x y) (f2 x y)
+interpQuilt env (Un op quilt) = undefined --- HERE
+interpQuilt env (Coordinates X) = Right $ \x _ -> [x,x,x]
+interpQuilt env (Coordinates Y) = Right $ \_ y -> [y,y,y]
+interpQuilt env (QuiltOp q1 q2 q3 q4) = undefined --- HERE
+interpQuilt env (Var name) = undefined --- HERE 
+interpQuilt env (Let var value expr) = undefined --- HERE 
+
 
 ---------------
 ---- Eval -----
 ---------------
 
 evalQuilt :: String -> Either String QuiltFun
-evalQuilt input = case parse quilt input of
-  Left err -> Left (show err)       --ERROR Message
-  Right expr -> case interpQuilt M.empty expr of 
-        Left l -> Left (showInterpError l) 
-        Right r -> Right r 
+evalQuilt s = case parse quilt s of
+    Left err -> Left (show err) -- FIXME
+    Right expr -> case inferType expr of
+        Left typeErr -> Left $ showTypeError typeErr
+        Right _ -> case interpQuilt M.empty expr of
+            Left interpErr -> Left $ showInterpError interpErr
+            Right f -> Right f
 
 -- evalQuilt :: String -> Either String QuiltFun
 -- evalQuilt s = case parse quilt s of
@@ -211,32 +325,3 @@ evalQuilt input = case parse quilt input of
 
 
 
--- % > calc :: String -> String
--- % > calc input = case parse (parseArith <* eof) input of         
--- % >          Left l -> show l                               -- if there's a parse error, show it
--- % >          Right r -> case interpArith M.empty r of       -- interpret it
--- % >                      Left err -> showInterpError err    -- if there's an interpretation error, show it
--- % >                      Right answer -> prettyPrint r ++ "\n   = " ++ printf "%f" answer 
--- % >                      -- everything works, do the math, display it 
-
--- > calc2 :: String -> String
--- > calc2 input = case parse (parseArith <* eof) input of 
--- >         Left l -> show l 
--- >         --Right expr ->  case inferType expr of 
--- >              --  Left l -> showTypeError l 
--- >         Right expr -> case interpArith M.empty expr of 
--- >                 Left err -> showInterpError err 
--- >                 Right answer ->  showAs answer 
-
-
-parseQuilt :: Parser Quilt
-parseQuilt = buildExpressionParser table parseQuiltAtom
-  where
-    table = [ 
-            [ Infix (Bin Times <$ reservedOp "*") AssocLeft
-              , Infix (Bin Divide   <$ reservedOp "/") AssocLeft
-              ]
-            , [ Infix (Bin Plus  <$ reservedOp "+") AssocLeft
-              , Infix (Bin Minus <$ reservedOp "-") AssocLeft
-              ]
-              ]
